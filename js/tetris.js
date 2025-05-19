3ee124ffbf88c4f1a16228e9fd756b5936c1db7b
ryusei172 <241205172@ccmailg.meijo-u.ac.jp>
2025-05-19 14:34:38 +0900

    blueをcian

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'blue', 'orange', 'skyblue', 'yellow', 'red', 'lime', 'purple'
+    'cian', 'orange', 'skyblue', 'yellow', 'red', 'lime', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
